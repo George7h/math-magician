f07f52dcc6c474fbee720c78cd65d5ba8fad71e8
George <georgie.george7@gmail.com>
2023-08-19 22:07:47 +0400

    restructure page, add basic layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import Calculator from './components/calculator';
+import GetQuote from './components/getQuote';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
+    <div>
+      <nav className="navbar">
+        <div className="appName">
+          <h1>Math Magicians</h1>
+        </div>
+        <ul className="nav-list">
+          <li><a href="#home">Home</a></li>
+          <li>|</li>
+          <li><a href="#calculator">Calculator</a></li>
+          <li>|</li>
+          <li><a href="#quote">Quote</a></li>
+        </ul>
+      </nav>
+      <div className="contain">
+        <div className="quoteApp">
+          <GetQuote />
+        </div>
+        <div className="calculatorApp">
+          <Calculator />
+        </div>
+      </div>
     </div>
   );
 }
